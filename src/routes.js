cfaf6585077186470e10bb45ff661a4c8256ae5e
yonkow <42253121+yonkow@users.noreply.github.com>
2024-01-21 15:43:55 +0200

    Add 404 page

--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,10 @@ const homeController = require('./controllers/homeController');
 const movieController = require('./controllers/movieController');
 
 router.use(homeController);
-
 router.use(movieController);
+
+router.get('*', (req, res) => {
+    res.redirect('/404');
+});
 
 module.exports = router;
