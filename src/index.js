71927f8e2d96cb2c018b532d849b05e9807570de
yonkow <42253121+yonkow@users.noreply.github.com>
2024-01-21 13:32:13 +0200

    Configure express-handlebars

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
-const express = require("express");
-const app = express();
-
+const express = require('express');
+const handlebars = require('express-handlebars');
+const path = require('path');
 const port = 5000;
 
-app.get("/", (req, res) => {
-    res.send("Home page");
+const app = express();
+app.engine(
+    'hbs',
+    handlebars.engine({
+        extname: 'hbs',
+    }));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', (req, res) => {
+    res.render('home', { layout: false });
 });
 
 app.listen(port, () => {
