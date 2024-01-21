8d691655e5c75179d5c59f0e65ab971fd5c0b3d1
yonkow <42253121+yonkow@users.noreply.github.com>
2024-01-21 15:28:47 +0200

    Add main layout

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-    res.render('home', { layouts: false } );
+    res.render('home');
+});
 });
 
 module.exports = router;
