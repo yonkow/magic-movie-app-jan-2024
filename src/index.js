99870bf8b9e1f480d6578c0e054383c84d80e22d
yonkow <42253121+yonkow@users.noreply.github.com>
2024-01-21 12:16:14 +0200

    Setup express server

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+const port = 5000;
+
+app.get("/", (req, res) => {
+    res.send("Home page");
+});
+
+app.listen(port, () => {
+    console.log(`Server is listening on port: ${port}...`);
+});
