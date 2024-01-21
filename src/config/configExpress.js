963b645806dac193f10e78ce5ad6be0631fd74fb
yonkow <42253121+yonkow@users.noreply.github.com>
2024-01-21 16:51:53 +0200

    Create movie action and service

--- src/config/configExpress.js
+++ src/config/configExpress.js
@@ -2,11 +2,10 @@ const express = require('express');
 const path = require('path');
 
 function configExpress(app) {
-
     app.use(express.static(path.resolve('src/public')));
+    app.use(express.urlencoded({ extended: false }));
 
     return app;
-
-};
+}
 
 module.exports = configExpress;
