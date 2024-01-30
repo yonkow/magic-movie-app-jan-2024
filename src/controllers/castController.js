46e399b10f42ef38eb1331220f02c2648fdd7485
yonkow <42253121+yonkow@users.noreply.github.com>
2024-01-30 22:37:18 +0200

    Add post action for cast create

--- src/controllers/castController.js
+++ src/controllers/castController.js
@@ -4,4 +4,13 @@ router.get('/create', (req, res) => {
     res.render('cast/create');
 });
 
+router.post('/create', (req, res) => {
+    const data = req.body;
+
+    console.log(data);
+
+    res.redirect('/');
+
+});
+
 module.exports = router;
