3976a3caf62d73f6118919a72f4bf27e971d941b
yonkow <42253121+yonkow@users.noreply.github.com>
2024-02-07 12:41:35 +0200

    Add registered user to DB

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -6,5 +6,22 @@ router.get('/register', (req, res) => {
     res.render('auth/register');
 })
 
+router.post('/register', async (req, res) => {
+    const regData = req.body;
+
+    const hash = await bc.hash(req.body.password, 12);
+
+    regData.password = hash;
+
+    try {
+        await authService.register(regData);
+        res.redirect('/auth/login');
+        
+    } catch (error) {
+        console.log(error.message);
+        res.redirect('/auth/register');
+    }
+
+});
 
 module.exports = router;
---
+++ src/models/User.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        lowercase: true,
+    },
+    password: {
+        type: String,
+        require: true,
+    },
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
