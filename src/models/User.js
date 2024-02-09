b439fe6a530abb4248b9c7966f075d93f5543391
yonkow <42253121+yonkow@users.noreply.github.com>
2024-02-09 14:00:45 +0200

    add validation

--- src/models/User.js
+++ src/models/User.js
@@ -6,10 +6,15 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
         lowercase: true,
-    },
+        unique: true,
+        match: [/@[a-zA-Z0-9]+\.[a-zA-Z0-9]+$/, 'Email should be valid.'],
+        minLength: [10, 'Email should be at least 10 characters long'],
+    }, 
     password: {
         type: String,
         require: true,
+        match: [/\w+/, 'Password should consist only of English letters and digits'],
+        minLength: [6, 'Password should be at least 6 characters long'],
     },
 });
 
