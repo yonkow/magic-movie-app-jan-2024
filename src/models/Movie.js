50b63f9ecd43cb6fed995c0395c5b2d47942494c
yonkow <42253121+yonkow@users.noreply.github.com>
2024-02-09 15:03:13 +0200

    Add Movie model validation

--- src/models/Movie.js
+++ src/models/Movie.js
@@ -6,37 +6,44 @@ const movieSchema = new mongoose.Schema({
     title: {
         type: String,
         required: true,
+        match: [/\w.+/, 'Title should be alphanumerical and could includes spaces.'],
+        minLength: [5, 'Title is too short'],
     },
     genre: {
         type: String,
         required: true,
         lowercase: true,
+        match: [/\w.+/, 'Genre should be alphanumerical and could includes spaces.'],
+        minLength: [5, 'Genre is too short'],
     },
     director: {
         type: String,
         required: true,
+        match: [/\w.+/, 'Director should be alphanumerical and could includes spaces.'],
+        minLength: [5, 'Director is too short'],
     },
     year: {
         type: Number,
         reqired: true,
-        min: 1888,
+        min: 1900,
         max: maxReleasedYear,
     },
     rating: {
         type: Number,
         required: true,
-        min: 1,
-        max: 5,
+        min: [1, 'The rating should be between 1 and 5'],
+        max: [5, 'The rating should be between 1 and 5'],
     },
     description: {
         type: String,
         required: true,
-        maxLength: 1000,
+        match: [/\w.+/, 'Description should be alphanumerical and could includes spaces.'],
+        minLength: [20, 'Description is too short'],
     },
     imageUrl: {
         type: String,
         required: true,
-        match: /^https?:\/\//
+        match: [/^https?:\/\//, 'URL should be in valid format http/https...' ]
     },
     casts: [{
         type: mongoose.Types.ObjectId,
